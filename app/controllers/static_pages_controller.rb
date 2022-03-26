af55260c83555158a0a25f326209ae593a6588d9
dvnDC <damcichosz@gmail.com>
2022-03-26 04:56:23 +0100

    Add friends method to static pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,14 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @micropost = current_user.microposts.build
-      @feed_microposts = current_user.feed.paginate(page: params[:page], :per_page => 12)
+      @feed_microposts = current_user.feed.paginate(page: params[:page], :per_page => 8)
+    end
+  end
+
+  def friends
+    if logged_in?
+      @micropost = current_user.microposts.build
+      @feed_microposts = current_user.feed.paginate(page: params[:page], :per_page => 8)
     end
   end
 
