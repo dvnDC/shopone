7930b2754e8eee36374c60dc9ad4f59650a791b1
dvnDC <damcichosz@gmail.com>
2022-04-01 04:17:02 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get    '/help',     to: 'static_pages#help'
   get    '/friends',  to: 'static_pages#friends'
   get    '/contact',  to: 'static_pages#contact'
+  get    '/about',    to: 'static_pages#about'
   get    '/signup',   to: 'users#new'
   get    '/login',    to: 'sessions#new'
   post   '/login',    to: 'sessions#create'
