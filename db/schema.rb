36ec0ad323985b89e623882fe951cf98b552c2ac
dvnDC <damcichosz@gmail.com>
2022-04-08 03:21:49 +0200

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_08_005440) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_08_010646) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,12 +60,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_08_005440) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
-    t.integer "category"
     t.decimal "price", precision: 5, scale: 2
     t.text "content"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "subcategory_id"
+    t.index ["subcategory_id"], name: "index_items_on_subcategory_id"
     t.index ["user_id", "created_at"], name: "index_items_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
@@ -111,6 +112,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_08_005440) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "items"
   add_foreign_key "comments", "users"
+  add_foreign_key "items", "subcategories"
   add_foreign_key "items", "users"
   add_foreign_key "microposts", "users"
   add_foreign_key "subcategories", "categories"
