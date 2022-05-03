cea1d1582c33da95214bdbafc6b572c4bc7c97db
dvnDC <damcichosz@gmail.com>
2022-05-03 23:32:16 +0200

    Create subcategories controller with show action

--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -1,3 +1,14 @@
 class SubcategoriesController < ApplicationController
+  def show
+    @subcategory = Subcategory.find(params[:id])
+    @items = @subcategory.items.paginate(page: params[:page], :per_page => 16)
+    @subcategories = @items
+  end
+
+  private
+
+  def subcategory_params
+    params.require(:subcategory)
+  end
 end
 
